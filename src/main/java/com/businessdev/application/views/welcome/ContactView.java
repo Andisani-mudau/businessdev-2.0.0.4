416671be694a8fe9de958fb1000eaaeedb71b15f
andisani-mudau <andisanimudau101@gmail.com>
2024-11-13 09:50:56 +0200

    added views and updated layout

---
+++ src/main/java/com/businessdev/application/views/welcome/ContactView.java
@@ -0,0 +1,25 @@
+package com.businessdev.application.views.welcome;
+
+import com.businessdev.application.views.welcome.MainLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.Style;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("contact")
+@Route(value = "contact", layout = MainLayout.class)
+public class ContactView extends VerticalLayout {
+
+}
