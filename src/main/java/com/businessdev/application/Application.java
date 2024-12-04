2d62878d31500202bc65d995bc6d5360867931bc
andisani-mudau <andisanimudau101@gmail.com>
2024-12-04 17:31:42 +0200

    Remove loadConfiguration method from Application class to streamline initialization process. This change eliminates the previous logic for moving a JSON file from the secrets directory to the resources folder, simplifying the application startup and reducing potential file handling errors.

--- src/main/java/com/businessdev/application/Application.java
+++ src/main/java/com/businessdev/application/Application.java
@@ -5,11 +5,7 @@ import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import org.json.JSONObject;
-import org.json.JSONException;
-import java.nio.file.Path;
+
 
 /**
  * The entry point of the Spring Boot application.
@@ -28,31 +24,5 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        loadConfiguration();
-    }
-
-    private static void loadConfiguration() {
-        try {
-            String sourcePath = "/etc/secrets/businessdev-calender-api-5ddd33a1bed7.json";
-            String content = new String(Files.readAllBytes(Paths.get(sourcePath)));
-            JSONObject jsonObject = new JSONObject(content);
-            // Use the jsonObject as needed
-
-            // Define the target path in the resources folder
-            String targetPath = "src/main/resources/businessdev-calender-api-5ddd33a1bed7.json";
-            
-            // Ensure the target directory exists
-            Path targetDir = Paths.get("src/main/resources");
-            if (!Files.exists(targetDir)) {
-                Files.createDirectories(targetDir);
-            }
-
-            // Move the file
-            Files.move(Paths.get(sourcePath), Paths.get(targetPath));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 }
