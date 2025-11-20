a2077f03b428bd3eea797ba5eac0822ab09ae957
Andisani <andisanimudau101@gmail.com>
2025-11-20 04:56:15 +0200

    Refactor MainNavigation to remove background color checks for navigation links
    - Simplified the styling of navigation links in MainNavigation.java by removing background color checks based on current location.
    - Standardized the styling approach for all navigation items, enhancing code clarity and maintainability.

--- src/main/java/com/businessdev/application/views/components/MainNavigation.java
+++ src/main/java/com/businessdev/application/views/components/MainNavigation.java
@@ -231,28 +231,23 @@ public class MainNavigation extends VerticalLayout{
 
         Anchor home = new Anchor(" ", homeContainer);
         home.setClassName("link");
-        home.getStyle().setBackgroundColor(isCurrentLocation("") ? "var(--lumo-shade-10pct)" : "transparent")
-                .setBorderRadius("5px").setPadding("10px");
+        home.getStyle().setBorderRadius("5px").setPadding("10px");
 
         Anchor offers = new Anchor("offers", offersContainer);
         offers.setClassName("link");
-        offers.getStyle().setBackgroundColor(isCurrentLocation("offers") ? "var(--lumo-shade-10pct)" : "transparent")
-                .setBorderRadius("5px").setPadding("10px");
+        offers.getStyle().setBorderRadius("5px").setPadding("10px");
 
         Anchor services = new Anchor("survey", servicesContainer);
         services.setClassName("link");
-        services.getStyle().setBackgroundColor(isCurrentLocation("survey") ? "var(--lumo-shade-10pct)" : "transparent")
-                .setBorderRadius("5px").setPadding("10px");
+        services.getStyle().setBorderRadius("5px").setPadding("10px");
 
         Anchor about = new Anchor("about", aboutContainer);
         about.setClassName("link");
-        about.getStyle().setBackgroundColor(isCurrentLocation("about") ? "var(--lumo-shade-10pct)" : "transparent")
-                .setBorderRadius("5px").setPadding("10px");
+        about.getStyle().setBorderRadius("5px").setPadding("10px");
 
         Anchor contact = new Anchor("contact", contactContainer);
         contact.setClassName("link");
-        contact.getStyle().setBackgroundColor(isCurrentLocation("contact") ? "var(--lumo-shade-10pct)" : "transparent")
-                .setBorderRadius("5px").setPadding("10px");
+        contact.getStyle().setBorderRadius("5px").setPadding("10px");
         navSection2.add(home, offers, contact, about);
         //...
         TextField chat = new TextField();
